3e6f20a7490e516ce83273d37485226d91b4d81b
sivasaipushadapu <60134058+sivasaipushadapu@users.noreply.github.com>
2021-12-13 17:31:25 +0530

    Update settings.py

--- authtest/settings.py
+++ authtest/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-!q9k65&2y1*l21brsmj58by8udn++)#p)5e-56%(gvjc6v%%9_
 DEBUG = False
 BASE_URL = "https://healthis.herokuapp.com"
 
-ALLOWED_HOSTS = ['localhost', 'healthis.herokuapp.com']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
